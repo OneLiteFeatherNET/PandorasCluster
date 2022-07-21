d72dd14b0039f42bf51e8ad45f5fcdd465cb819a
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-07-22 00:03:16 +0200

    added chunk util methods from Minestom

---
+++ src/main/java/net/onelitefeather/pandorascluster/util/ChunkUtil.java
@@ -0,0 +1,46 @@
+package net.onelitefeather.pandorascluster.util;
+
+import org.bukkit.Chunk;
+import org.jetbrains.annotations.NotNull;
+
+public class ChunkUtil {
+
+    /**
+     * Gets the chunk index of chunk coordinates.
+     * <p>
+     * Used when you want to store a chunk somewhere without using a reference to the whole object
+     * (as this can lead to memory leaks).
+     *
+     * @param chunkX the chunk X
+     * @param chunkZ the chunk Z
+     * @return a number storing the chunk X and Z
+     */
+    public static long getChunkIndex(int chunkX, int chunkZ) {
+        return (((long) chunkX) << 32) | (chunkZ & 0xffffffffL);
+    }
+
+    public static long getChunkIndex(@NotNull Chunk chunk) {
+        return getChunkIndex(chunk.getX(), chunk.getZ());
+    }
+
+    /**
+     * Converts a chunk index to its chunk X position.
+     *
+     * @param index the chunk index computed by {@link #getChunkIndex(int, int)}
+     * @return the chunk X based on the index
+     */
+    public static int getChunkCoordX(long index) {
+        return (int) (index >> 32);
+    }
+
+    /**
+     * Converts a chunk index to its chunk Z position.
+     *
+     * @param index the chunk index computed by {@link #getChunkIndex(int, int)}
+     * @return the chunk Z based on the index
+     */
+    public static int getChunkCoordZ(long index) {
+        return (int) index;
+    }
+
+}
