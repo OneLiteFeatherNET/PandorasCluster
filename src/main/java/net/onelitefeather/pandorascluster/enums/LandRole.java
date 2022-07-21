2c1cb5b7c1323a993cb1db2a995d2aa1d328989d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-07-22 00:04:16 +0200

    added access field

--- src/main/java/net/onelitefeather/pandorascluster/enums/LandRole.java
+++ src/main/java/net/onelitefeather/pandorascluster/enums/LandRole.java
@@ -7,21 +7,23 @@ import java.util.Map;
 
 public enum LandRole {
 
-    OWNER("owner", "64Owner"),
-    ADMIN("admin", "&cAdmin"),
-    TRUSTED("trusted", "&aTrusted"),
-    MEMBER("member", "&eMember"),
-    BANNED("banned", "&c&oBanned"),
-    VISITOR("visitor", "&7Visitor");
+    OWNER("owner", "64Owner", true),
+    ADMIN("admin", "&cAdmin", true),
+    TRUSTED("trusted", "&aTrusted", true),
+    MEMBER("member", "&eMember", true),
+    BANNED("banned", "&c&oBanned", false),
+    VISITOR("visitor", "&7Visitor", false);
 
     private final String name;
     private final String display;
+    private final boolean access;
 
     private static final Map<String, LandRole> BY_NAME = Maps.newHashMap();
 
-    LandRole(String name, String display) {
+    LandRole(String name, String display, boolean access) {
         this.name = name;
         this.display = display;
+        this.access = access;
     }
 
     @NotNull
@@ -32,6 +34,10 @@ public enum LandRole {
     @NotNull
     public String getDisplay() {
         return display;
+    }
+
+    public boolean hasAccess() {
+        return access;
     }
 
     public static LandRole getChunkRole(String name) {
