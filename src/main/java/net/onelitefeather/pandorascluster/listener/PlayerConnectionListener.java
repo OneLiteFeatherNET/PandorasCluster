decdfbcd11367ef5fc6cfd79fc7e54394f568c6a
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-07-22 00:19:36 +0200

    Improved code

--- src/main/java/net/onelitefeather/pandorascluster/listener/PlayerConnectionListener.java
+++ src/main/java/net/onelitefeather/pandorascluster/listener/PlayerConnectionListener.java
@@ -1,30 +1,18 @@
 package net.onelitefeather.pandorascluster.listener;
 
-import net.onelitefeather.pandorascluster.PandorasClusterPlugin;
-import net.onelitefeather.pandorascluster.player.ChunkPlayer;
-import org.bukkit.entity.Player;
+import net.onelitefeather.pandorascluster.PandorasClusterApi;
+import net.onelitefeather.pandorascluster.land.player.LandPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public record PlayerConnectionListener(PandorasClusterPlugin pandorasClusterPlugin) implements Listener {
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        this.pandorasClusterPlugin.getChunkPlayerService().loadOnlinePlayer(event.getPlayer());
-    }
+public record PlayerConnectionListener(PandorasClusterApi api) implements Listener {
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-
-        ChunkPlayer chunkPlayer = this.pandorasClusterPlugin.getChunkPlayerService().getChunkPlayer(player.getUniqueId());
-        if (chunkPlayer != null) {
-            this.pandorasClusterPlugin.getChunkPlayerService().updateChunkPlayer(chunkPlayer);
+        LandPlayer landPlayer = this.api.getLandPlayerService().getLandPlayer(event.getPlayer().getUniqueId());
+        if (landPlayer != null) {
+            this.api.getLandPlayerService().updateLandPlayer(landPlayer);
         }
-
-
-        this.pandorasClusterPlugin.getChunkPlayerService().removeOnlinePlayer(player);
     }
 }
