a63f4b8e697503c9801d288095e5e3dae47a87d1
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-07-22 00:35:16 +0200

    changed class to record

--- src/main/java/net/onelitefeather/pandorascluster/land/flag/LandFlagHandler.java
+++ src/main/java/net/onelitefeather/pandorascluster/land/flag/LandFlagHandler.java
@@ -3,14 +3,7 @@ package net.onelitefeather.pandorascluster.land.flag;
 import net.onelitefeather.pandorascluster.land.Land;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 
-public final class LandFlagHandler {
-
-    private final Land land;
-
-    public LandFlagHandler(Land land) {
-        this.land = land;
-    }
-
+public record LandFlagHandler(Land land) {
 
     public void handleEntityChangeBlock(EntityChangeBlockEvent event) {
         LandFlagEntity landFlag = this.land.getFlag(LandFlag.BLOCK_FORM);
