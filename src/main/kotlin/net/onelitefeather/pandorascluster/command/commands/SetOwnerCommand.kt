0a66dfa9815ddf3c54e0c5e16df980f5e80ac062
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-13 22:55:24 +0200

    Improve code readability

--- src/main/kotlin/net/onelitefeather/pandorascluster/command/commands/SetOwnerCommand.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/command/commands/SetOwnerCommand.kt
@@ -1,11 +1,8 @@
 package net.onelitefeather.pandorascluster.command.commands
 
-import cloud.commandframework.annotations.Argument
-import cloud.commandframework.annotations.CommandDescription
-import cloud.commandframework.annotations.CommandMethod
-import cloud.commandframework.annotations.CommandPermission
+import cloud.commandframework.annotations.*
 import net.onelitefeather.pandorascluster.api.PandorasClusterApi
-import net.onelitefeather.pandorascluster.extensions.toMM
+import net.onelitefeather.pandorascluster.extensions.miniMessage
 import net.onelitefeather.pandorascluster.land.player.LandPlayer
 import org.bukkit.entity.Player
 
@@ -14,20 +11,32 @@ class SetOwnerCommand(private val pandorasClusterApi: PandorasClusterApi) {
     @CommandMethod("land setowner <player>")
     @CommandPermission("pandorascluster.command.land.setowner")
     @CommandDescription("Set the Owner of the Land")
+    @Confirmation
     fun execute(player: Player, @Argument("player", parserName = "landPlayer") landPlayer: LandPlayer) {
 
-        val land = pandorasClusterApi.landService.getFullLand(player.chunk)
-        if (land == null) {
-            player.sendMessage("Nichts gefunden!".toMM())
+        val land = pandorasClusterApi.getLandService().getFullLand(player.chunk)
+        if(land == null) {
+            player.sendMessage(miniMessage { "Du musst auf deinem Land stehen" })
             return
         }
 
-        if(land.isOwner(landPlayer.uniqueId)) {
-            player.sendMessage("Nothing changed player ${landPlayer.name} is already the land owner".toMM())
+        val playerId = landPlayer.getUniqueId();
+        if (playerId == null) {
+            player.sendMessage(miniMessage { "Der Spieler ${landPlayer.name} existiert nicht" })
             return
         }
 
-        this.pandorasClusterApi.landService.setLandOwner(land, landPlayer)
-        player.sendMessage("Owner changed".toMM())
+        if(land.isOwner(playerId)) {
+            player.sendMessage(miniMessage { "Nothing changed player ${landPlayer.name} is already the land owner" })
+            return
+        }
+
+        if(land.getLandMember(playerId) != null) {
+            player.sendMessage(miniMessage { "The player ${landPlayer.name} is already a member of this Land" })
+            return
+        }
+
+        this.pandorasClusterApi.getDatabaseStorageService().setLandOwner(land, landPlayer)
+        player.sendMessage(miniMessage { "Owner changed" })
     }
 }
