ead3ec63416adba2c4e76b5d552bba4248b1f2f1
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-13 22:36:33 +0200

    Improve code flow and make code more readable

--- src/main/kotlin/net/onelitefeather/pandorascluster/command/commands/SetFlagCommand.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/command/commands/SetFlagCommand.kt
@@ -3,11 +3,12 @@ package net.onelitefeather.pandorascluster.command.commands
 import cloud.commandframework.annotations.Argument
 import cloud.commandframework.annotations.CommandMethod
 import cloud.commandframework.annotations.CommandPermission
+import cloud.commandframework.annotations.Confirmation
 import cloud.commandframework.annotations.specifier.Quoted
 import net.onelitefeather.pandorascluster.api.PandorasClusterApi
-import net.onelitefeather.pandorascluster.extensions.toMM
+import net.onelitefeather.pandorascluster.extensions.miniMessage
 import net.onelitefeather.pandorascluster.land.flag.LandFlagEntity
-import net.onelitefeather.pandorascluster.util.DUMMY_FLAG_ENTITY
+import net.onelitefeather.pandorascluster.util.Constants
 import org.bukkit.entity.Player
 
 class SetFlagCommand(private val pandorasClusterApi: PandorasClusterApi) {
@@ -15,24 +16,25 @@ class SetFlagCommand(private val pandorasClusterApi: PandorasClusterApi) {
 
     @CommandMethod("land flag set <flag> <value>")
     @CommandPermission("pandorascluster.command.land.flag.set")
+    @Confirmation
     fun execute(
         player: Player,
         @Argument("flag", parserName = "landFlag") landFlagEntity: LandFlagEntity,
         @Argument(value = "value") @Quoted value: String
     ) {
 
-        val land = pandorasClusterApi.landService.getFullLand(player.chunk)
+        val land = pandorasClusterApi.getLandService().getFullLand(player.chunk)
         if (land == null) {
             player.sendMessage("Nichts gefunden!".toMM())
             return
         }
 
-        if(landFlagEntity == DUMMY_FLAG_ENTITY) {
-            player.sendMessage("The flag not exists".toMM())
+        if(landFlagEntity == Constants.DUMMY_FLAG_ENTITY) {
+            player.sendMessage(miniMessage { "The flag not exists" })
             return
         }
 
-        pandorasClusterApi.landService.updateLandFlag(landFlagEntity.copy(value = value))
-        player.sendMessage("New value $value")
+        pandorasClusterApi.getDatabaseStorageService().updateLandFlag(landFlagEntity.copy(value = value))
+        player.sendMessage(miniMessage { "New value $value" })
     }
 }
