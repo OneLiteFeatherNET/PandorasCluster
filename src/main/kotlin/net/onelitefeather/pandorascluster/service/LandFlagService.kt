165a7008b47a88d554ebf56c1d221e366aa943c5
Dustin <uniquegame@onelitefeather.net>
2022-09-01 18:04:02 +0000

    Feature/91 chunk protection

---
+++ src/main/kotlin/net/onelitefeather/pandorascluster/service/LandFlagService.kt
@@ -0,0 +1,79 @@
+package net.onelitefeather.pandorascluster.service
+
+import io.sentry.Sentry
+import net.onelitefeather.pandorascluster.api.PandorasClusterApi
+import net.onelitefeather.pandorascluster.land.Land
+import net.onelitefeather.pandorascluster.land.flag.LAND_FLAGS
+import net.onelitefeather.pandorascluster.land.flag.LandFlag
+import net.onelitefeather.pandorascluster.land.flag.LandFlagEntity
+import net.onelitefeather.pandorascluster.util.DUMMY_LAND
+import org.hibernate.HibernateException
+import java.util.logging.Level
+
+class LandFlagService(private val pandorasClusterApi: PandorasClusterApi) {
+
+    fun getDefaultFlags(): List<LandFlagEntity> {
+        val list = arrayListOf<LandFlagEntity>()
+        LAND_FLAGS.forEach {
+            list.add(
+                LandFlagEntity(
+                    null,
+                    it.name,
+                    it.defaultValue.toString(),
+                    it.type,
+                    it.landFlagType,
+                    DUMMY_LAND
+                )
+            )
+        }
+
+        return list
+    }
+
+    fun getDefaultFlag(landFlag: LandFlag): LandFlagEntity {
+        return getDefaultFlags().first { it.name == landFlag.name }
+    }
+
+
+    fun existsFlagInLand(landFlag: LandFlag?, land: Land): Boolean {
+        return getLandFlag(landFlag ?: LandFlag.UNKNOWN, land) != null
+    }
+
+    fun getLandFlag(landFlag: LandFlag, land: Land): LandFlagEntity? {
+
+        try {
+            pandorasClusterApi.getSessionFactory().openSession().use { session ->
+                val flagOfLand = session.createQuery(
+                    "SELECT f FROM LandFlagEntity f JOIN FETCH f.land l WHERE l.id = :landId AND f.name = :name",
+                    LandFlagEntity::class.java
+                )
+                flagOfLand.setParameter("landId", land.id)
+                flagOfLand.setParameter("name", landFlag.name)
+                return flagOfLand.uniqueResult()
+            }
+        } catch (e: HibernateException) {
+            pandorasClusterApi.getLogger().log(Level.SEVERE, "Cannot load landflag $landFlag", e)
+            Sentry.captureException(e)
+        }
+
+        return getDefaultFlag(landFlag)
+    }
+
+    fun getFlagsByLand(land: Land): List<LandFlagEntity> {
+        try {
+            pandorasClusterApi.getSessionFactory().openSession().use { session ->
+                val flagsOfLand = session.createQuery(
+                    "SELECT f FROM LandFlagEntity f JOIN FETCH f.land l WHERE l.id = :landId",
+                    LandFlagEntity::class.java
+                )
+                flagsOfLand.setParameter("landId", land.id)
+                return flagsOfLand.list()
+            }
+        } catch (e: HibernateException) {
+            pandorasClusterApi.getLogger().log(Level.SEVERE, "Cannot load flags by land", e)
+            Sentry.captureException(e)
+        }
+
+        return listOf()
+    }
+}
