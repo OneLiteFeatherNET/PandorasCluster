89dbebf237e15e8974e32862bdca03e418c757f7
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-14 22:54:31 +0200

    Improve gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     kotlin("jvm") version "1.7.10"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
     id("xyz.jpenilla.run-paper") version "1.0.6"
 
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.liquibase.gradle") version "2.1.0"
     id("org.sonarqube") version "3.4.0.2513"
     jacoco
-
-    id("io.papermc.paperweight.userdev") version "1.3.6"
 }
 
 group = "net.onelitefeather"
@@ -19,16 +17,13 @@ repositories {
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://libraries.minecraft.net")
-    maven("https://repo.dmulloy2.net/repository/public/")
     maven("https://jitpack.io")
     maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
-    paperDevBundle("1.19.2-R0.1-SNAPSHOT")
 
-    // Paper
-    compileOnly("com.comphenix.protocol:ProtocolLib:4.8.0")
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
 
     //WorldGuard
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.1.0-SNAPSHOT")
@@ -38,21 +33,21 @@ dependencies {
     }
 
     // Commands
-    implementation("cloud.commandframework", "cloud-paper", "1.7.1")
-    implementation("cloud.commandframework", "cloud-annotations", "1.7.1")
-    implementation("cloud.commandframework", "cloud-minecraft-extras", "1.7.1")
-    implementation("net.kyori:adventure-platform-bukkit:4.1.2")
-    implementation("me.lucko:commodore:2.2") {
+    bukkitLibrary("cloud.commandframework", "cloud-paper", "1.7.1")
+    bukkitLibrary("cloud.commandframework", "cloud-annotations", "1.7.1")
+    bukkitLibrary("cloud.commandframework", "cloud-minecraft-extras", "1.7.1")
+    bukkitLibrary("net.kyori:adventure-platform-bukkit:4.1.2")
+    bukkitLibrary("me.lucko:commodore:2.2") {
         isTransitive = false
     }
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    bukkitLibrary("com.github.ben-manes.caffeine:caffeine:3.1.1")
 
     // Sentry
-    implementation("org.apache.logging.log4j:log4j-core:2.18.0")
-    implementation(libs.sentry)
-    implementation(libs.sentryJul)
-    implementation(libs.sentrylog4j2)
+    bukkitLibrary("org.apache.logging.log4j:log4j-core:2.19.0")
+    bukkitLibrary(libs.sentry)
+    bukkitLibrary(libs.sentryJul)
+    bukkitLibrary(libs.sentrylog4j2)
 
     // Database
     implementation("org.hibernate:hibernate-core:6.1.3.Final")
@@ -95,7 +90,7 @@ kotlin {
 
 tasks {
     build {
-        dependsOn(reobfJar)
+        dependsOn(shadowJar)
     }
     compileKotlin {
         kotlinOptions {
@@ -120,6 +115,7 @@ tasks {
 
     runServer {
         minecraftVersion("1.19.2")
+        jvmArgs("-Xmx4G")
     }
 
     shadowJar {
@@ -134,7 +130,6 @@ bukkit {
     load = net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder.POSTWORLD
 
     authors = listOf("UniqueGame", "OneLiteFeather")
-    depend = listOf("ProtocolLib")
     softDepend = listOf("WorldGuard")
 }
 sonarqube {
