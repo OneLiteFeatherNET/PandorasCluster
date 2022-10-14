6edd93130988e2dd877c62af8efc73614f4976a7
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-14 22:59:28 +0200

    Remove private

--- src/main/kotlin/net/onelitefeather/pandorascluster/listener/PlayerConnectionListener.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/listener/PlayerConnectionListener.kt
@@ -6,7 +6,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
-class PlayerConnectionListener(private val api: PandorasClusterApi) : Listener {
+class PlayerConnectionListener(val api: PandorasClusterApi) : Listener {
 
     @EventHandler
     fun handlePlayerJoin(event: PlayerJoinEvent) {
