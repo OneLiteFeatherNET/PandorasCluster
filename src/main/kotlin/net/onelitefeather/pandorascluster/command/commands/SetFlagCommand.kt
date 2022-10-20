0cc64eecca8f998d6611c666036a21754ba86703
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-20 20:00:19 +0200

    Add check for validate the value and handle the use flag

--- src/main/kotlin/net/onelitefeather/pandorascluster/command/commands/SetFlagCommand.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/command/commands/SetFlagCommand.kt
@@ -8,6 +8,7 @@ import cloud.commandframework.annotations.specifier.Quoted
 import net.onelitefeather.pandorascluster.api.PandorasClusterApi
 import net.onelitefeather.pandorascluster.extensions.miniMessage
 import net.onelitefeather.pandorascluster.land.flag.LandFlag
+import net.onelitefeather.pandorascluster.land.flag.isValidValue
 import org.bukkit.entity.Player
 
 class SetFlagCommand(private val pandorasClusterApi: PandorasClusterApi) {
@@ -18,7 +19,7 @@ class SetFlagCommand(private val pandorasClusterApi: PandorasClusterApi) {
     fun execute(
         player: Player,
         @Argument("flag", parserName = "landFlag") landFlag: LandFlag,
-        @Argument(value = "value") @Quoted value: String
+        @Argument(value = "value", suggestions = "flag_values") @Quoted value: String
     ) {
         val pluginPrefix = pandorasClusterApi.pluginPrefix()
         val land = pandorasClusterApi.getLand(player.chunk)
@@ -27,12 +28,22 @@ class SetFlagCommand(private val pandorasClusterApi: PandorasClusterApi) {
             return
         }
 
+        if(!isValidValue(landFlag, value)) {
+            player.sendMessage(miniMessage { "The value $value is not valid for flag $landFlag" })
+            return
+        }
+
         if(landFlag == LandFlag.UNKNOWN) {
             player.sendMessage(miniMessage { pandorasClusterApi.i18n("command.set-flag.not-found", *arrayOf(pluginPrefix)) })
             return
         }
 
-        pandorasClusterApi.getDatabaseStorageService().updateLandFlag(landFlag, value, land)
+        if(landFlag != LandFlag.USE) {
+            pandorasClusterApi.getDatabaseStorageService().updateLandFlag(landFlag, value, land)
+        } else {
+            pandorasClusterApi.getDatabaseStorageService().addUseMaterial(land, value)
+
+        }
         player.sendMessage(miniMessage { pandorasClusterApi.i18n("command.set-flag.success", *arrayOf(pluginPrefix, landFlag.name, value)) })
     }
 }
