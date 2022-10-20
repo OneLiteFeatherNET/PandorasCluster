250a2d421986ed5e7d93e5c522e4873a127b1c7b
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-20 19:57:33 +0200

    Clean up code

--- src/main/kotlin/net/onelitefeather/pandorascluster/listener/entity/LandHangingEntityListener.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/listener/entity/LandHangingEntityListener.kt
@@ -2,6 +2,7 @@ package net.onelitefeather.pandorascluster.listener.entity
 
 import net.onelitefeather.pandorascluster.api.PandorasClusterApi
 import net.onelitefeather.pandorascluster.enums.Permission
+import net.onelitefeather.pandorascluster.extensions.hasPermission
 import net.onelitefeather.pandorascluster.land.flag.LandFlag
 import net.onelitefeather.pandorascluster.service.LandService
 import org.bukkit.event.EventHandler
@@ -22,20 +23,37 @@ class LandHangingEntityListener(private val pandorasClusterApi: PandorasClusterA
 
     @EventHandler
     fun handleHangingBreakByEntity(event: HangingBreakByEntityEvent) {
+
         val entity = event.entity
-        val land = pandorasClusterApi.getLand(entity.chunk) ?: return
+        val land = pandorasClusterApi.getLand(entity.chunk)
         val remover = event.remover
 
-        if(remover != null && (Permission.HANGING_BREAK.hasPermission(remover) || land.hasAccess(remover.uniqueId))) return
+        if(land == null) {
+            event.isCancelled = if(remover != null) {
+                !remover.hasPermission(Permission.UNOWNED_CHUNK)
+            } else {
+                false
+            }
+            return
+        }
+
+        if(remover != null && land.hasAccess(remover.uniqueId)) return
         event.isCancelled = land.getLandFlag(LandFlag.HANGING_BREAK).getValue<Boolean>() == false
     }
 
     @EventHandler
     fun handleHangingPlace(event: HangingPlaceEvent) {
-        val land = pandorasClusterApi.getLand(event.block.chunk) ?: return
-        val player = event.player
 
-        if(player != null && (Permission.HANGING_BREAK.hasPermission(player) || land.hasAccess(player.uniqueId))) return
-        event.isCancelled = land.getLandFlag(LandFlag.HANGING_PLACE).getValue<Boolean>() == false
+        val land = pandorasClusterApi.getLand(event.block.chunk)
+        val player = event.player
+        val landFlag = LandFlag.HANGING_PLACE
+
+        if(land == null && player != null) {
+            event.isCancelled = !player.hasPermission(Permission.UNOWNED_CHUNK)
+            return
+        }
+
+        if(player != null && (player.hasPermission(landFlag) || land?.hasAccess(player.uniqueId) == true)) return
+        event.isCancelled = land?.getLandFlag(landFlag)?.getValue<Boolean>() == false
     }
 }
