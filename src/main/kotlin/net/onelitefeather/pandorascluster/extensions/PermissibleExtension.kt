1a5baa1a9abc4c45b5daaf031cc85929ee012e52
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-20 19:58:21 +0200

    Add method hasPermission

--- src/main/kotlin/net/onelitefeather/pandorascluster/extensions/PermissibleExtension.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/extensions/PermissibleExtension.kt
@@ -1,6 +1,11 @@
 package net.onelitefeather.pandorascluster.extensions
 
 import net.onelitefeather.pandorascluster.enums.Permission
+import net.onelitefeather.pandorascluster.land.flag.LandFlag
 import org.bukkit.permissions.Permissible
+
+private const val basePermission = "pandorascluster.flags.flag.%s"
+
+fun Permissible.hasPermission(landFlag: LandFlag) = this.hasPermission(basePermission.format(landFlag.name))
 
 fun Permissible.hasPermission(permission: Permission) = this.hasPermission(permission.permissionNode)
