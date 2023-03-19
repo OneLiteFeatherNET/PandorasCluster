4ce5e86c2d47d24aa1e066b626a090e27cd9debc
TheMeinerLP <p.glanz@madfix.me>
2023-03-19 01:11:28 +0100

    Add missing flags

--- src/main/kotlin/net/onelitefeather/pandorascluster/land/flag/LandFlag.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/land/flag/LandFlag.kt
@@ -29,7 +29,24 @@ enum class LandFlag(val flagName: String,
     FARMLAND_DESTROY("farmland-destroy", true, LandFlagType.ENTITY, 2),
     MOB_GRIEFING("mob-griefing", true, LandFlagType.ENTITY, 2),
     ICE_FORM("ice-form", false, LandFlagType.WORLD_TICK, 2),
-    BLOCK_FORM("block-form", false, LandFlagType.ENTITY, 2);
+    BLOCK_FORM("block-form", false, LandFlagType.ENTITY, 2),
+    TURTLE_EGG_DESTROY("turtle-egg-destroy", false, LandFlagType.ENTITY, 2),
+    UNKNOWN("unknown", false, LandFlagType.ENTITY, 2),
+    INTERACT_CROPS("interact-crops", false, LandFlagType.PLAYER, 2),
+    ENTITY_MOUNT("entity-mount", false, LandFlagType.ENTITY, 2),
+    ENTITY_TAME("entity-tame", false, LandFlagType.ENTITY, 2),
+    BUCKET_INTERACT("bucket-interact", false, LandFlagType.PLAYER, 2),
+    SHEAR_BLOCK("shear-block", false, LandFlagType.PLAYER, 2),
+    SHEAR_ENTITY("shear-entity", false, LandFlagType.PLAYER, 2),
+    TAKE_LECTERN("take-lectern", false, LandFlagType.PLAYER, 2),
+    ENTITY_LEASH("entity-leash", false, LandFlagType.PLAYER, 2),
+    USE_BED("use-bed", false, LandFlagType.PLAYER, 2),
+    VILLAGER_INTERACT("villager-interact", false, LandFlagType.PLAYER, 2),
+    FIRE_PROTECTION("fire-protection", false, LandFlagType.WORLD_TICK, 2),
+    FARMLAND_DESTROY("farmland-destroy", true, LandFlagType.ENTITY, 2),
+    INTERACT_CONTAINERS("interact-containers", false, LandFlagType.PLAYER, 2),
+    SPONGE_ABSORB("sponge-absorb", false, LandFlagType.WORLD_TICK, 2);
+
 }
 
 val LAND_FLAGS = LandFlag.values()
