0a4119b6c77dc79933824720f64aafee6f6a7e79
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-04-14 17:47:11 +0200

    Add method canMountEntity

--- src/main/kotlin/net/onelitefeather/pandorascluster/extensions/EntityUtils.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/extensions/EntityUtils.kt
@@ -1,16 +1,30 @@
 package net.onelitefeather.pandorascluster.extensions
 
 import net.onelitefeather.pandorascluster.enums.Permission
+import net.onelitefeather.pandorascluster.land.Land
 import net.onelitefeather.pandorascluster.land.flag.LandFlag
 import net.onelitefeather.pandorascluster.util.IGNORE_CLAIM_LIMIT
 import org.apache.commons.lang3.StringUtils
 import org.bukkit.entity.AnimalTamer
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Tameable
 import org.bukkit.permissions.Permissible
 
 interface EntityUtils {
 
     fun isPetOwner(tameable: Tameable, animalTamer: AnimalTamer): Boolean = tameable.owner?.uniqueId == animalTamer.uniqueId
+
+    fun canMountEntity(mount: Entity, entity: Entity, land: Land): Boolean {
+
+        if (land.hasAccess(entity.uniqueId)) return true
+        if (land.getLandFlag(LandFlag.ENTITY_MOUNT).getValue<Boolean>() == true) return true
+
+        if (mount is Tameable && entity is AnimalTamer) {
+            return mount.isTamed && isPetOwner(mount, entity)
+        }
+
+        return hasPermission(entity, LandFlag.ENTITY_MOUNT)
+    }
 
     fun getHighestClaimLimit(permissible: Permissible): Int {
         if(hasPermission(permissible, Permission.NO_CLAIM_LIMIT)) return IGNORE_CLAIM_LIMIT
