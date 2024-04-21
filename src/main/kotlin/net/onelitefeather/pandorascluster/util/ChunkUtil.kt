d34b0b476ec38ccc6ef81de253ba1d3551b08142
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-04-21 21:47:06 +0200

    [pandoras-cluster#8] Rename class MobCapType to EntityCategory

--- src/main/kotlin/net/onelitefeather/pandorascluster/util/ChunkUtil.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/util/ChunkUtil.kt
@@ -1,6 +1,6 @@
 package net.onelitefeather.pandorascluster.util
 
-import net.onelitefeather.pandorascluster.api.MobCapType
+import net.onelitefeather.pandorascluster.api.EntityCategory
 import net.onelitefeather.pandorascluster.land.Land
 import net.onelitefeather.pandorascluster.land.flag.LandFlag
 import org.bukkit.Bukkit
@@ -10,38 +10,37 @@ import org.bukkit.entity.AbstractVillager
 import org.bukkit.entity.Animals
 import org.bukkit.entity.Monster
 import org.bukkit.entity.Player
-import org.bukkit.entity.SpawnCategory
 
 fun hasSameOwner(it: Land, claimedLand: Land) = it.owner == claimedLand.owner
 
-fun getEntityLimit(land: Land, mobCapType: MobCapType): Int {
+fun getEntityLimit(land: Land, entityCategory: EntityCategory): Int {
 
-    if(mobCapType == MobCapType.ANIMALS) {
+    if(entityCategory == EntityCategory.ANIMALS) {
         return land.getLandFlag(LandFlag.ANIMAL_CAP).getValue() ?: 0
     }
 
-    if(mobCapType == MobCapType.MONSTER) {
+    if(entityCategory == EntityCategory.MONSTER) {
         return land.getLandFlag(LandFlag.MONSTER_CAP).getValue() ?: 0
     }
 
-    if(mobCapType == MobCapType.VILLAGER) {
+    if(entityCategory == EntityCategory.VILLAGER) {
         return land.getLandFlag(LandFlag.VILLAGER_CAP).getValue() ?: 0
     }
 
     return 0
 }
 
-fun getEntityCount(land: Land, mobCapType: MobCapType): Int {
+fun getEntityCount(land: Land, entityCategory: EntityCategory): Int {
 
     var count = 0
     val world = Bukkit.getWorld(land.world) ?: return 0
 
     land.chunks.forEach {
         val bukkitChunk = world.getChunkAt(it.chunkIndex)
-        count += when(mobCapType) {
-            MobCapType.ANIMALS -> bukkitChunk.entities.filterIsInstance<Animals>().size
-            MobCapType.MONSTER -> bukkitChunk.entities.filterIsInstance<Monster>().size
-            MobCapType.VILLAGER -> bukkitChunk.entities.filterIsInstance<AbstractVillager>().size
+        count += when(entityCategory) {
+            EntityCategory.ANIMALS -> bukkitChunk.entities.filterIsInstance<Animals>().size
+            EntityCategory.MONSTER -> bukkitChunk.entities.filterIsInstance<Monster>().size
+            EntityCategory.VILLAGER -> bukkitChunk.entities.filterIsInstance<AbstractVillager>().size
         }
     }
 
