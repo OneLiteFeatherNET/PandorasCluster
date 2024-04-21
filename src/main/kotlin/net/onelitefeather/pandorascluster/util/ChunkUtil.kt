68adf48aced6c3a9afffbea74fe0aa342fffae3e
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-04-21 21:33:57 +0200

    [pandoras-cluster#8] Add methods to control the entity spawn limit

--- src/main/kotlin/net/onelitefeather/pandorascluster/util/ChunkUtil.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/util/ChunkUtil.kt
@@ -1,11 +1,53 @@
 package net.onelitefeather.pandorascluster.util
 
+import net.onelitefeather.pandorascluster.api.MobCapType
 import net.onelitefeather.pandorascluster.land.Land
+import net.onelitefeather.pandorascluster.land.flag.LandFlag
+import org.bukkit.Bukkit
 import org.bukkit.Chunk
 import org.bukkit.block.Block
+import org.bukkit.entity.AbstractVillager
+import org.bukkit.entity.Animals
+import org.bukkit.entity.Monster
 import org.bukkit.entity.Player
+import org.bukkit.entity.SpawnCategory
 
 fun hasSameOwner(it: Land, claimedLand: Land) = it.owner == claimedLand.owner
+
+fun getEntityLimit(land: Land, mobCapType: MobCapType): Int {
+
+    if(mobCapType == MobCapType.ANIMALS) {
+        return land.getLandFlag(LandFlag.ANIMAL_CAP).getValue() ?: 0
+    }
+
+    if(mobCapType == MobCapType.MONSTER) {
+        return land.getLandFlag(LandFlag.MONSTER_CAP).getValue() ?: 0
+    }
+
+    if(mobCapType == MobCapType.VILLAGER) {
+        return land.getLandFlag(LandFlag.VILLAGER_CAP).getValue() ?: 0
+    }
+
+    return 0
+}
+
+fun getEntityCount(land: Land, mobCapType: MobCapType): Int {
+
+    var count = 0
+    val world = Bukkit.getWorld(land.world) ?: return 0
+
+    land.chunks.forEach {
+        val bukkitChunk = world.getChunkAt(it.chunkIndex)
+        count += when(mobCapType) {
+            MobCapType.ANIMALS -> bukkitChunk.entities.filterIsInstance<Animals>().size
+            MobCapType.MONSTER -> bukkitChunk.entities.filterIsInstance<Monster>().size
+            MobCapType.VILLAGER -> bukkitChunk.entities.filterIsInstance<AbstractVillager>().size
+        }
+    }
+
+    return count
+}
+
 
 fun chunkCorners(chunk: Chunk): List<Block> {
     val world = chunk.world
