94a3243d3288ea22e34533781da900d299986b08
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-04-28 20:02:27 +0200

    Remove unused file

--- src/main/kotlin/net/onelitefeather/pandorascluster/extensions/StringExtension.kt
+++
@@ -1,5 +0,0 @@
-package net.onelitefeather.pandorascluster.extensions
-
-import net.kyori.adventure.text.minimessage.MiniMessage
-
-fun miniMessage(message: () -> String) = MiniMessage.miniMessage().deserialize(message())
