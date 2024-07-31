d33ef5678932e7a888d0cee484266201221c6c1d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-08-01 00:02:06 +0200

    Cleanup

--- src/main/kotlin/net/onelitefeather/pandorascluster/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/util/Constants.kt
@@ -5,22 +5,29 @@ import net.onelitefeather.pandorascluster.land.Land
 import net.onelitefeather.pandorascluster.land.flag.LandFlagEntity
 import net.onelitefeather.pandorascluster.land.flag.LandFlagType
 import net.onelitefeather.pandorascluster.land.position.dummyHomePosition
-import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.block.BlockFace
 import java.util.*
 
-const val PREFIX = "[§aPandorasLand§r]"
-const val PLUGIN_NAME = "PandorasCluster"
-const val CHUNK_LENGTH = 16
-
 const val IGNORE_CLAIM_LIMIT: Int = -2
 
+val MATERIALS = Material.entries.toTypedArray()
+val BLOCK_FACES = BlockFace.entries.toTypedArray()
+val CHUNK_ROTATIONS = ChunkRotation.entries.toTypedArray()
+
 val propertyDiscordAvatarUrl: String = System.getProperty("discordAvatarUrl", "https://mc-heads.net/avatar/%s/100")
-val propertyDiscordWebhookUrl: String = System.getProperty("discordWebhookUrl", "https://discord.com/api/webhooks/%s/%s")
+val propertyDiscordWebhookUrl: String =
+    System.getProperty("discordWebhookUrl", "https://discord.com/api/webhooks/%s/%s")
 
 val SERVER_UUID: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
 val EVERYONE: UUID = UUID.fromString("1-1-3-3-7")
+
+val AVAILABLE_CHUNK_ROTATIONS = arrayOf(
+    BlockFace.NORTH,
+    BlockFace.EAST,
+    BlockFace.SOUTH,
+    BlockFace.WEST
+)
 
 val DUMMY_LAND = Land(
     -1,
@@ -33,27 +40,6 @@ val DUMMY_LAND = Land(
     -1, -1
 )
 
-fun getChunkRotation(name: String): ChunkRotation? {
-    return CHUNK_ROTATIONS.firstOrNull { chunkRotation:
-                                                  ChunkRotation ->
-        chunkRotation.name.equals(name, true)
-    }
-}
-
-fun getBlockFace(name: String): BlockFace? {
-    return BLOCK_FACES.firstOrNull { blockFace -> blockFace.name.equals(name, true) }
-}
-
-fun getBlockFace(location: Location): BlockFace? {
-    return BLOCK_FACES.firstOrNull { blockFace ->  blockFace.direction == location.direction}
-}
-
-val AVAILABLE_CHUNK_ROTATIONS = arrayOf(
-    BlockFace.NORTH,
-    BlockFace.EAST,
-    BlockFace.SOUTH,
-    BlockFace.WEST)
-
 val DUMMY_FLAG_ENTITY = LandFlagEntity(
     -1,
     "dummy",
@@ -63,11 +49,5 @@ val DUMMY_FLAG_ENTITY = LandFlagEntity(
     DUMMY_LAND
 )
 
-val MATERIALS = Material.values()
-val BLOCK_FACES = BlockFace.values()
-val CHUNK_ROTATIONS = ChunkRotation.values()
 
-fun getChunkRotation(facing: BlockFace) : ChunkRotation? {
-    return CHUNK_ROTATIONS.firstOrNull { it.name == facing.name }
-}
 
