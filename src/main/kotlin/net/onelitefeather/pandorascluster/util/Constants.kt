5d3a42340a22908a68cc5214261d40a15a777442
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-08-01 00:15:44 +0200

    Remove unused fields Add comment for unused fields

--- src/main/kotlin/net/onelitefeather/pandorascluster/util/Constants.kt
+++ src/main/kotlin/net/onelitefeather/pandorascluster/util/Constants.kt
@@ -1,10 +1,6 @@
 package net.onelitefeather.pandorascluster.util
 
 import net.onelitefeather.pandorascluster.enums.ChunkRotation
-import net.onelitefeather.pandorascluster.land.Land
-import net.onelitefeather.pandorascluster.land.flag.LandFlagEntity
-import net.onelitefeather.pandorascluster.land.flag.LandFlagType
-import net.onelitefeather.pandorascluster.land.position.dummyHomePosition
 import org.bukkit.Material
 import org.bukkit.block.BlockFace
 import java.util.*
@@ -19,6 +15,7 @@ val propertyDiscordAvatarUrl: String = System.getProperty("discordAvatarUrl", "h
 val propertyDiscordWebhookUrl: String =
     System.getProperty("discordWebhookUrl", "https://discord.com/api/webhooks/%s/%s")
 
+// Both varibles are getting used in later releases
 val SERVER_UUID: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
 val EVERYONE: UUID = UUID.fromString("1-1-3-3-7")
 
@@ -27,26 +24,6 @@ val AVAILABLE_CHUNK_ROTATIONS = arrayOf(
     BlockFace.EAST,
     BlockFace.SOUTH,
     BlockFace.WEST
-)
-
-val DUMMY_LAND = Land(
-    -1,
-    null,
-    dummyHomePosition(),
-    arrayListOf(),
-    arrayListOf(),
-    arrayListOf(),
-    "world",
-    -1, -1
-)
-
-val DUMMY_FLAG_ENTITY = LandFlagEntity(
-    -1,
-    "dummy",
-    "dummy",
-    0,
-    LandFlagType.ENTITY,
-    DUMMY_LAND
 )
 
 
