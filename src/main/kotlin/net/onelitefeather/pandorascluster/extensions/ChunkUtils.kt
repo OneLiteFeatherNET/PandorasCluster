bf5af120c18999006cf719ae9bca9b1e158df27a
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-08-01 00:01:08 +0200

    Convert to an interface

---
+++ src/main/kotlin/net/onelitefeather/pandorascluster/extensions/ChunkUtils.kt
@@ -0,0 +1,86 @@
+package net.onelitefeather.pandorascluster.extensions
+
+import net.onelitefeather.pandorascluster.api.EntityCategory
+import net.onelitefeather.pandorascluster.enums.ChunkRotation
+import net.onelitefeather.pandorascluster.enums.Permission
+import net.onelitefeather.pandorascluster.land.Land
+import net.onelitefeather.pandorascluster.land.flag.LandFlag
+import net.onelitefeather.pandorascluster.util.BLOCK_FACES
+import net.onelitefeather.pandorascluster.util.CHUNK_ROTATIONS
+import org.bukkit.Bukkit
+import org.bukkit.Chunk
+import org.bukkit.Location
+import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
+import org.bukkit.entity.AbstractVillager
+import org.bukkit.entity.Animals
+import org.bukkit.entity.Monster
+import org.bukkit.entity.Player
+
+interface ChunkUtils {
+
+    fun canEnterLand(player: Player, land: Land): Boolean {
+        return Permission.LAND_ENTRY_DENIED.hasPermission(player) || !land.isBanned(player.uniqueId)
+    }
+
+    fun hasSameOwner(it: Land, claimedLand: Land) = it.owner == claimedLand.owner
+
+    fun getEntityLimit(land: Land, entityCategory: EntityCategory): Int {
+
+        if(entityCategory == EntityCategory.ANIMALS) {
+            return land.getLandFlag(LandFlag.ANIMAL_CAP).getValue() ?: 0
+        }
+
+        if(entityCategory == EntityCategory.MONSTER) {
+            return land.getLandFlag(LandFlag.MONSTER_CAP).getValue() ?: 0
+        }
+
+        if(entityCategory == EntityCategory.VILLAGER) {
+            return land.getLandFlag(LandFlag.VILLAGER_CAP).getValue() ?: 0
+        }
+
+        return 0
+    }
+
+    fun getEntityCount(land: Land, entityCategory: EntityCategory): Int {
+
+        var count = 0
+        val world = Bukkit.getWorld(land.world) ?: return 0
+
+        land.chunks.forEach {
+            val bukkitChunk = world.getChunkAt(it.chunkIndex)
+            count += when(entityCategory) {
+                EntityCategory.ANIMALS -> bukkitChunk.entities.filterIsInstance<Animals>().size
+                EntityCategory.MONSTER -> bukkitChunk.entities.filterIsInstance<Monster>().size
+                EntityCategory.VILLAGER -> bukkitChunk.entities.filterIsInstance<AbstractVillager>().size
+            }
+        }
+
+        return count
+    }
+
+    fun chunkCorners(chunk: Chunk): List<Block> {
+        val world = chunk.world
+        val chunkX = chunk.x
+        val chunkZ = chunk.z
+        return listOf(
+            world.getHighestBlockAt(chunkX * 16, chunkZ * 16),
+            world.getHighestBlockAt(chunkX * 16 + 15, chunkZ * 16),
+            world.getHighestBlockAt(chunkX * 16, chunkZ * 16 + 15),
+            world.getHighestBlockAt(chunkX * 16 + 15, chunkZ * 16 + 15)
+        )
+    }
+
+    fun getPlayersInChunk(chunk: Chunk): List<Player> = chunk.entities.filterIsInstance<Player>()
+
+    fun chunkLength() = 16
+
+    fun getChunkRotation(facing: BlockFace): ChunkRotation? {
+        return CHUNK_ROTATIONS.firstOrNull { it.name == facing.name }
+    }
+
+    fun getBlockFace(location: Location): BlockFace? {
+        return BLOCK_FACES.firstOrNull { it.direction == location.direction }
+    }
+
+}
