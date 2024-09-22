bf7fc12a4d5fffe6e1a65f65fa15e2395da65bcd
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-22 20:21:14 +0200

    Add file

---
+++ plugin/src/main/kotlin/net/onelitefeather/pandorascluster/util/BukkitPlayerUtil.kt
@@ -0,0 +1,13 @@
+package net.onelitefeather.pandorascluster.util
+
+import net.onelitefeather.pandorascluster.api.utils.PlayerUtil
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import java.util.*
+
+class BukkitPlayerUtil : PlayerUtil {
+
+    override fun getPlayer(uuid: UUID): Player? = Bukkit.getPlayer(uuid)
+
+    override fun isOnline(uuid: UUID): Boolean = getPlayer(uuid) != null
+}
