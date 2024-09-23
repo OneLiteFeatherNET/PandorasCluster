8beb17b39d264985e56f97d7823e5835cf075dbf
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-23 20:49:45 +0200

    Add database entity for the land home

--- adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/position/HomePositionEntity.kt
+++ adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/position/HomePositionEntity.kt
@@ -1,10 +1,10 @@
 package net.onelitefeather.pandorascluster.database.models.position
 
 import jakarta.persistence.*
-import org.hibernate.Hibernate
 
 @Entity
-data class HomePosition(
+@Table(name = "land_homes")
+data class HomePositionEntity(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,14 +26,6 @@ data class HomePosition(
     val pitch: Float = 1.0F
 ) {
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
-        other as HomePosition
-
-        return id == other.id
-    }
-
     override fun hashCode(): Int = javaClass.hashCode()
 
     @Override
@@ -41,4 +33,18 @@ data class HomePosition(
         this::class.simpleName +
                 "(id = $id , posX = $posX , posY = $posY , posZ = $posZ , " +
                 "yaw = $yaw , pitch = $pitch)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is HomePositionEntity) return false
+
+        if (id != other.id) return false
+        if (posX != other.posX) return false
+        if (posY != other.posY) return false
+        if (posZ != other.posZ) return false
+        if (yaw != other.yaw) return false
+        if (pitch != other.pitch) return false
+
+        return true
+    }
 }
