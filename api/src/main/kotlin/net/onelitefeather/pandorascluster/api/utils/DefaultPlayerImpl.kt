1aa978923f8b7bf3a9b4bf191d48b37dadca1608
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-23 20:57:54 +0200

    Add default implementation

--- api/src/main/kotlin/net/onelitefeather/pandorascluster/api/utils/DefaultPlayerImpl.kt
+++ api/src/main/kotlin/net/onelitefeather/pandorascluster/api/utils/DefaultPlayerImpl.kt
@@ -1,5 +1,15 @@
 package net.onelitefeather.pandorascluster.api.utils
 
+import java.util.*
+
 class DefaultPlayerImpl : PlayerUtil {
+    override fun getPlayer(uuid: UUID): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun isOnline(uuid: UUID): Boolean {
+        TODO("Not yet implemented")
+    }
+
 
 }
