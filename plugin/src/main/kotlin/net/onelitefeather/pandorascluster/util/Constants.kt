0f715f932b772b664a4bc58ccfe310646326593b
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-23 20:52:30 +0200

    Clean up file

--- plugin/src/main/kotlin/net/onelitefeather/pandorascluster/util/Constants.kt
+++ plugin/src/main/kotlin/net/onelitefeather/pandorascluster/util/Constants.kt
@@ -1,42 +1,18 @@
 package net.onelitefeather.pandorascluster.util
 
-import net.onelitefeather.pandorascluster.enums.ChunkRotation
-import net.onelitefeather.pandorascluster.land.Land
-import net.onelitefeather.pandorascluster.land.position.dummyHomePosition
+import net.onelitefeather.pandorascluster.api.enums.ChunkRotation
 import org.bukkit.Material
 import org.bukkit.block.BlockFace
-import java.util.*
-
-const val IGNORE_CLAIM_LIMIT: Int = -2
 
 val MATERIALS = Material.entries.toTypedArray()
 val BLOCK_FACES = BlockFace.entries.toTypedArray()
 val CHUNK_ROTATIONS = ChunkRotation.entries.toTypedArray()
-
-val propertyDiscordAvatarUrl: String = System.getProperty("discordAvatarUrl", "https://mc-heads.net/avatar/%s/100")
-val propertyDiscordWebhookUrl: String =
-    System.getProperty("discordWebhookUrl", "https://discord.com/api/webhooks/%s/%s")
-
-// Both varibles are getting used in later releases
-val SERVER_UUID: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
-val EVERYONE: UUID = UUID.fromString("1-1-3-3-7")
 
 val AVAILABLE_CHUNK_ROTATIONS = arrayOf(
     BlockFace.NORTH,
     BlockFace.EAST,
     BlockFace.SOUTH,
     BlockFace.WEST
-)
-
-val DUMMY_LAND = Land(
-    -1,
-    null,
-    dummyHomePosition(),
-    arrayListOf(),
-    arrayListOf(),
-    arrayListOf(),
-    "world",
-    -1, -1
 )
 
 
