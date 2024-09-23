e21faeaf9d250fcc48280641d2be6b210d743b88
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-23 20:58:24 +0200

    Add file

--- api/src/main/kotlin/net/onelitefeather/pandorascluster/api/service/LandService.kt
+++ api/src/main/kotlin/net/onelitefeather/pandorascluster/api/service/LandService.kt
@@ -1,4 +1,54 @@
 package net.onelitefeather.pandorascluster.api.service
 
+import net.onelitefeather.pandorascluster.api.land.Land
+import net.onelitefeather.pandorascluster.api.chunk.ClaimedChunk
+import net.onelitefeather.pandorascluster.api.player.LandPlayer
+import net.onelitefeather.pandorascluster.api.position.HomePosition
+import java.util.*
+
 interface LandService {
+
+    fun getLands(): List<Land>
+
+    /**
+     * @param homePosition the home position of the land
+     * @param ownerId the new owner uuid
+     */
+    fun updateLandHome(homePosition: HomePosition, ownerId: UUID)
+
+    /**
+     * @param land the land to update
+     */
+    fun updateLand(land: Land)
+
+    /**
+     * @param chunk the chunk to claim
+     * @param land the land to add the chunk
+     */
+    fun addClaimedChunk(chunk: ClaimedChunk, land: Land?)
+
+    /**
+     * @param owner the owner of the land.
+     * @param player the bukkit player
+     * @param chunk the first claimed chunk
+     */
+    fun createLand(owner: LandPlayer, home: HomePosition, chunk: ClaimedChunk, world: String)
+
+    /**
+     * @param land the land to unclaim.
+     */
+    fun unclaimLand(land: Land)
+
+    /**
+     * @param claimedChunk the chunk to remove from the land.
+     */
+    fun removeClaimedChunk(chunk: ClaimedChunk)
+
+    fun getLand(chunk: ClaimedChunk): Land?
+
+    fun getLand(owner: LandPlayer): Land?
+
+    fun isChunkClaimed(chunk: ClaimedChunk): Boolean
+
+    fun hasPlayerLand(player: LandPlayer): Boolean
 }
