2121f4401592fd1105b80edea8b7fd4703369210
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-23 21:23:30 +0200

    Throw a not implemented exception

--- api/src/main/kotlin/net/onelitefeather/pandorascluster/api/utils/DefaultPlayerImpl.kt
+++ api/src/main/kotlin/net/onelitefeather/pandorascluster/api/utils/DefaultPlayerImpl.kt
@@ -1,14 +1,15 @@
 package net.onelitefeather.pandorascluster.api.utils
 
+import jdk.jshell.spi.ExecutionControl.NotImplementedException
 import java.util.*
 
 class DefaultPlayerImpl : PlayerUtil {
     override fun getPlayer(uuid: UUID): Any {
-        TODO("Not yet implemented")
+        throw NotImplementedException("This is not implemented yet!")
     }
 
     override fun isOnline(uuid: UUID): Boolean {
-        TODO("Not yet implemented")
+        throw NotImplementedException("This is not implemented yet!")
     }
 
 
