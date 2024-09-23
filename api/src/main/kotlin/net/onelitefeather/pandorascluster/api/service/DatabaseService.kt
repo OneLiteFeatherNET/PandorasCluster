24be1499903ecb06dd4239598ba292bbdf1d2a3a
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-09-23 20:58:24 +0200

    Add file

--- api/src/main/kotlin/net/onelitefeather/pandorascluster/api/service/DatabaseService.kt
+++ api/src/main/kotlin/net/onelitefeather/pandorascluster/api/service/DatabaseService.kt
@@ -1,4 +1,23 @@
 package net.onelitefeather.pandorascluster.api.service
 
+import net.onelitefeather.pandorascluster.database.mapper.impl.*
+import org.hibernate.SessionFactory
+import java.nio.file.Path
+
 interface DatabaseService {
+
+    fun connect(configFilePath: Path)
+
+    fun shutdown()
+
+    fun isRunning(): Boolean
+
+    fun sessionFactory(): SessionFactory
+
+    fun landMapper(): LandEntityMapper
+    fun landMemberMapper(): LandMemberMapper
+    fun landPlayerMapper(): LandPlayerMapper
+    fun claimedChunkMapper(): ClaimedChunkMapper
+    fun homePositionMapper(): HomePositionMapper
+    fun flagMapper(): FlagRoleAttachmentMapper
 }
