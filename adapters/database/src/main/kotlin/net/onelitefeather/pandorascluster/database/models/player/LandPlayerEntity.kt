d254657f4c51097c92f2489e50136f21fa76342f
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-10-06 21:38:43 +0200

    Implement the wrapper for the api

--- adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/player/LandPlayerEntity.kt
+++ adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/player/LandPlayerEntity.kt
@@ -1,7 +1,7 @@
 package net.onelitefeather.pandorascluster.database.models.player
 
 import jakarta.persistence.*
-import net.onelitefeather.pandorascluster.api.utils.SERVER_UUID
+import net.onelitefeather.pandorascluster.dbo.player.LandPlayerDBO
 
 @Entity
 @Table(name = "land_players")
@@ -12,16 +12,24 @@ data class LandPlayerEntity(
     val id: Long? = null,
 
     @Column(columnDefinition = "VARCHAR(36)")
-    val uuid: String = SERVER_UUID.toString(),
+    val uuid: String? = null,
 
     @Column
     val name: String? = null
-) {
+): LandPlayerDBO {
+
+    constructor() : this(null, "", "")
 
     @Override
     override fun toString(): String {
         return this::class.simpleName + "(id = $id , uuid = $uuid , name = $name )"
     }
+
+    override fun id(): Long? = id
+
+    override fun uuid(): String = uuid ?: ""
+
+    override fun name(): String = name ?: ""
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
