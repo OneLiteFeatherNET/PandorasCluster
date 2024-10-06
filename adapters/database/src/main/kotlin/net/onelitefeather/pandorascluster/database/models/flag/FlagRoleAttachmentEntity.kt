7ad24a15a33677972d9d386e7bee8939001ed9d2
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-10-06 21:37:21 +0200

    Implement the wrapper for the api

--- adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/flag/FlagRoleAttachmentEntity.kt
+++ adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/flag/FlagRoleAttachmentEntity.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.*
 import net.onelitefeather.pandorascluster.api.enums.LandRole
 import net.onelitefeather.pandorascluster.api.land.flag.LandFlag
 import net.onelitefeather.pandorascluster.database.models.LandEntity
+import net.onelitefeather.pandorascluster.dbo.flag.FlagRoleAttachmentDBO
 
 @Entity
 @Table(name = "land_flags")
@@ -18,23 +19,28 @@ data class FlagRoleAttachmentEntity(
     val role: LandRole,
 
     @Column
-    var value: String = "null",
-
-    @Column
     @Enumerated(EnumType.STRING)
-    val flag: LandFlag = LandFlag.UNKNOWN,
+    val landFlag: LandFlag = LandFlag.UNKNOWN,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "land_id")
     val land: LandEntity? = null
-) {
+) : FlagRoleAttachmentDBO {
+
+    constructor() : this(null, LandRole.VISITOR, LandFlag.UNKNOWN)
+
+    override fun id(): Long? = id
+
+    override fun role(): LandRole = role
+
+    override fun flag(): LandFlag = landFlag
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is FlagRoleAttachmentEntity) return false
 
         if (id != other.id) return false
-        if (flag != other.flag) return false
+        if (landFlag != other.landFlag) return false
         if (role != other.role) return false
         if (land != other.land) return false
 
@@ -43,7 +49,7 @@ data class FlagRoleAttachmentEntity(
 
     override fun hashCode(): Int {
         var result = id?.hashCode() ?: 0
-        result = 31 * result + flag.hashCode()
+        result = 31 * result + landFlag.hashCode()
         result = 31 * result + role.hashCode()
         result = 31 * result + (land?.hashCode() ?: 0)
         return result
