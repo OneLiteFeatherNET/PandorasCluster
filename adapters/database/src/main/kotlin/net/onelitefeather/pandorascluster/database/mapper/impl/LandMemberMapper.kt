9e28c1b6fa07d0e151541b8fad61939e45b99b9d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-10-06 21:41:22 +0200

    Implement the dbo wrapper for the api

--- adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/mapper/impl/LandMemberMapper.kt
+++ adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/mapper/impl/LandMemberMapper.kt
@@ -1,23 +1,25 @@
 package net.onelitefeather.pandorascluster.database.mapper.impl
 
+import net.onelitefeather.pandorascluster.api.mapper.DatabaseEntityMapper
 import net.onelitefeather.pandorascluster.api.player.LandMember
-import net.onelitefeather.pandorascluster.database.mapper.DatabaseEntityMapper
+import net.onelitefeather.pandorascluster.api.service.DatabaseService
 import net.onelitefeather.pandorascluster.database.models.player.LandMemberEntity
-import net.onelitefeather.pandorascluster.database.service.DatabaseServiceImpl
+import net.onelitefeather.pandorascluster.database.models.player.LandPlayerEntity
+import net.onelitefeather.pandorascluster.dbo.player.LandMemberDBO
 
-class LandMemberMapper(private val databaseService: DatabaseServiceImpl) :
-    DatabaseEntityMapper<LandMemberEntity, LandMember> {
+class LandMemberMapper(private val databaseService: DatabaseService) :
+    DatabaseEntityMapper<LandMemberDBO, LandMember> {
 
-    override fun entityToModel(entity: LandMemberEntity?): LandMember? {
+    override fun entityToModel(entity: LandMemberDBO?): LandMember? {
         if (entity == null) return null
 
-        val member = databaseService.landPlayerMapper().entityToModel(entity.member) ?: return null
-        return LandMember(entity.id, member, entity.role)
+        val member = databaseService.landPlayerMapper().entityToModel(entity.member()) ?: return null
+        return LandMember(entity.id(), member, entity.role())
     }
 
-    override fun modelToEntity(model: LandMember?): LandMemberEntity? {
+    override fun modelToEntity(model: LandMember?): LandMemberDBO? {
         if (model == null) return null
         val member = databaseService.landPlayerMapper().modelToEntity(model.member) ?: return null
-        return LandMemberEntity(model.id, member, model.role)
+        return LandMemberEntity(model.id, member as LandPlayerEntity, model.role)
     }
 }
