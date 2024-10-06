66c1079ae18fc9e8481b17f3748f34b28e586f2d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-10-06 21:38:16 +0200

    Implement the wrapper for the api

--- adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/LandEntity.kt
+++ adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/models/LandEntity.kt
@@ -5,6 +5,12 @@ import net.onelitefeather.pandorascluster.database.models.flag.FlagRoleAttachmen
 import net.onelitefeather.pandorascluster.database.models.player.LandMemberEntity
 import net.onelitefeather.pandorascluster.database.models.player.LandPlayerEntity
 import net.onelitefeather.pandorascluster.database.models.position.HomePositionEntity
+import net.onelitefeather.pandorascluster.dbo.chunk.ClaimedChunkDBO
+import net.onelitefeather.pandorascluster.dbo.flag.FlagRoleAttachmentDBO
+import net.onelitefeather.pandorascluster.dbo.land.LandDBO
+import net.onelitefeather.pandorascluster.dbo.player.LandMemberDBO
+import net.onelitefeather.pandorascluster.dbo.player.LandPlayerDBO
+import net.onelitefeather.pandorascluster.dbo.position.HomePositionDBO
 
 @Entity
 @Table(name = "lands")
@@ -22,14 +28,15 @@ data class LandEntity(
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "land")
     val members: List<LandMemberEntity> = arrayListOf(),
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "land")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "landEntity")
     val chunks: List<ClaimedChunkEntity> = arrayListOf(),
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "land")
     val flags: List<FlagRoleAttachmentEntity> = arrayListOf(),
 
     @Column
-    val world: String = "world") {
+    val world: String = "world"
+) : LandDBO {
 
     @Override
     override fun toString(): String {
@@ -37,6 +44,20 @@ data class LandEntity(
                 "(id = $id , owner = $owner , homePosition = $home , " +
                 "world = $world)"
     }
+
+    override fun id(): Long? = id
+
+    override fun owner(): LandPlayerDBO = owner!!
+
+    override fun home(): HomePositionDBO = home!!
+
+    override fun members(): List<LandMemberDBO> = members
+
+    override fun chunks(): List<ClaimedChunkDBO> = chunks
+
+    override fun flags(): List<FlagRoleAttachmentDBO> = flags
+
+    override fun world(): String = world
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
