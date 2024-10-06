c5dd9f1644f639b4b30a76eea5f1025380fc0cce
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-10-06 21:41:44 +0200

    Implement the dbo wrapper for the api

--- api/src/main/kotlin/net/onelitefeather/pandorascluster/api/service/DatabaseService.kt
+++ api/src/main/kotlin/net/onelitefeather/pandorascluster/api/service/DatabaseService.kt
@@ -1,12 +1,23 @@
 package net.onelitefeather.pandorascluster.api.service
 
-import net.onelitefeather.pandorascluster.database.mapper.impl.*
+import net.onelitefeather.pandorascluster.api.chunk.ClaimedChunk
+import net.onelitefeather.pandorascluster.api.land.Land
+import net.onelitefeather.pandorascluster.api.land.flag.FlagRoleAttachment
+import net.onelitefeather.pandorascluster.api.mapper.DatabaseEntityMapper
+import net.onelitefeather.pandorascluster.api.player.LandMember
+import net.onelitefeather.pandorascluster.api.player.LandPlayer
+import net.onelitefeather.pandorascluster.api.position.HomePosition
+import net.onelitefeather.pandorascluster.dbo.chunk.ClaimedChunkDBO
+import net.onelitefeather.pandorascluster.dbo.flag.FlagRoleAttachmentDBO
+import net.onelitefeather.pandorascluster.dbo.land.LandDBO
+import net.onelitefeather.pandorascluster.dbo.player.LandMemberDBO
+import net.onelitefeather.pandorascluster.dbo.player.LandPlayerDBO
+import net.onelitefeather.pandorascluster.dbo.position.HomePositionDBO
 import org.hibernate.SessionFactory
-import java.nio.file.Path
 
 interface DatabaseService {
 
-    fun connect(configFilePath: Path)
+    fun connect(configFileResource: String)
 
     fun shutdown()
 
@@ -14,10 +25,10 @@ interface DatabaseService {
 
     fun sessionFactory(): SessionFactory
 
-    fun landMapper(): LandEntityMapper
-    fun landMemberMapper(): LandMemberMapper
-    fun landPlayerMapper(): LandPlayerMapper
-    fun claimedChunkMapper(): ClaimedChunkMapper
-    fun homePositionMapper(): HomePositionMapper
-    fun flagMapper(): FlagRoleAttachmentMapper
+    fun landMapper(): DatabaseEntityMapper<LandDBO, Land>
+    fun landMemberMapper(): DatabaseEntityMapper<LandMemberDBO, LandMember>
+    fun landPlayerMapper(): DatabaseEntityMapper<LandPlayerDBO, LandPlayer>
+    fun claimedChunkMapper(): DatabaseEntityMapper<ClaimedChunkDBO, ClaimedChunk>
+    fun homePositionMapper(): DatabaseEntityMapper<HomePositionDBO, HomePosition>
+    fun flagMapper(): DatabaseEntityMapper<FlagRoleAttachmentDBO, FlagRoleAttachment>
 }
