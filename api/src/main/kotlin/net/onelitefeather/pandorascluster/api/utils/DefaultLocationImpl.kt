19dc79e5beae9b975cdf84c41caedcd9936791c4
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-10-27 21:36:16 +0100

    Remove platform specific location support

--- api/src/main/kotlin/net/onelitefeather/pandorascluster/api/utils/DefaultLocationImpl.kt
+++
@@ -1,7 +0,0 @@
-package net.onelitefeather.pandorascluster.api.utils
-
-class DefaultLocationImpl : LocationUtil {
-    override fun locToBlock(input: Double): Int {
-        TODO("Not yet implemented")
-    }
-}
