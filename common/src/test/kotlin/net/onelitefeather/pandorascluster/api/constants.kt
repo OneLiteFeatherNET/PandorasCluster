c3c5ee37d53725e3f882c2cd23b3c34ef671e34d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-12-05 18:06:12 +0100

    Add constants for unit testings

--- common/src/test/kotlin/net/onelitefeather/pandorascluster/api/constants.kt
+++ common/src/test/kotlin/net/onelitefeather/pandorascluster/api/constants.kt
@@ -7,7 +7,7 @@ import java.util.*
 val updatedHome = HomePosition(null, 185.0, 27.0, 75.0, 0.0F, 0.0F)
 val landHome = HomePosition(null, 180.0, 25.0, 60.0, 0.0F, 0.0F)
 val mainChunk = ClaimedChunk(null, -12455436)
-val landOwnerUUID: UUID = UUID.fromString("05bf52c6-7bb0-4f13-8951-0e1fd803df35")
+val landOwnerUUID: UUID = UUID.fromString("df7c66a6-7876-44c2-9abd-9bd33f7d3d9e")
 
 const val randomBound = 199999999L
 val random = Random()
