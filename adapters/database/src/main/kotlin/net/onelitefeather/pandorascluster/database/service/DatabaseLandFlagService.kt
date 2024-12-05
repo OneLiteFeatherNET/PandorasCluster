03b30a2790b4728a8d9cb2c64f6c36a0ecf4c4aa
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2024-12-05 18:10:06 +0100

    Refactor code to improve the read-ability

--- adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/service/DatabaseLandFlagService.kt
+++ adapters/database/src/main/kotlin/net/onelitefeather/pandorascluster/database/service/DatabaseLandFlagService.kt
@@ -13,28 +13,32 @@ import net.onelitefeather.pandorascluster.database.models.flag.FlagRoleAttachmen
 import org.hibernate.HibernateException
 import org.hibernate.Transaction
 
-class DatabaseLandFlagService(private var databaseService: DatabaseService,
-                              private val databaseLandService: LandService) : LandFlagService {
+class DatabaseLandFlagService(
+    private var databaseService: DatabaseService,
+    private val landService: LandService
+) : LandFlagService {
 
     override fun addLandFlag(landFlag: LandFlag, role: LandRole?, land: Land) {
         if (land.hasFlag(landFlag)) return
         var transaction: Transaction? = null
+
         try {
             databaseService.sessionFactory().openSession().use { session ->
                 transaction = session.beginTransaction()
 
-                val flag = FlagRoleAttachmentEntity(
-                    null,
-                    role ?: LandRole.MEMBER,
-                    landFlag,
-                    databaseService.landMapper().modelToEntity(land) as LandEntity?
-                )
-
-                session.persist(flag)
-                transaction?.commit()
+                val landEntity = databaseService.landMapper().modelToEntity(land) as LandEntity?
+                if(landEntity != null) {
+                    val flag = FlagRoleAttachmentEntity(
+                        null,
+                        role ?: LandRole.MEMBER,
+                        landFlag,
+                        landEntity)
+                    session.persist(flag)
+                    transaction?.commit()
+                }
             }
 
-            databaseLandService.updateLoadedChunks(land)
+            landService.updateLand(land)
         } catch (e: HibernateException) {
             transaction?.rollback()
             LOGGER.throwing(this::class.java.simpleName, "addLandFlag", e)
@@ -42,13 +46,13 @@ class DatabaseLandFlagService(private var databaseService: DatabaseService,
     }
 
     override fun updateLandFlag(flag: FlagRoleAttachment, land: Land) {
-
         var transaction: Transaction? = null
         try {
             databaseService.sessionFactory().openSession().use { session ->
                 transaction = session.beginTransaction()
 
                 val landEntity = databaseService.landMapper().modelToEntity(land) as LandEntity
+
                 val flagEntity = (databaseService.flagMapper().modelToEntity(flag) as FlagRoleAttachmentEntity).copy(
                     land = landEntity
                 )
@@ -57,7 +61,7 @@ class DatabaseLandFlagService(private var databaseService: DatabaseService,
                 transaction?.commit()
             }
 
-            databaseLandService.updateLoadedChunks(land)
+            landService.updateLand(land)
         } catch (e: HibernateException) {
             transaction?.rollback()
             LOGGER.throwing(this::class.java.simpleName, "updateLandFlag", e)
@@ -79,45 +83,10 @@ class DatabaseLandFlagService(private var databaseService: DatabaseService,
                 transaction?.commit()
             }
 
-            databaseLandService.updateLoadedChunks(land)
+            landService.updateLand(land)
         } catch (e: HibernateException) {
             transaction?.rollback()
             LOGGER.throwing(this::class.java.simpleName, "updateLandFlag", e)
         }
     }
-
-    override fun getLandFlag(landFlag: LandFlag, land: Land): FlagRoleAttachment? {
-        try {
-            databaseService.sessionFactory().openSession().use { session ->
-                val flagOfLand = session.createQuery(
-                    "SELECT f FROM FlagRoleAttachmentEntity f JOIN FETCH f.land l WHERE l.id = :landId AND f.landFlag = :landFlag",
-                    FlagRoleAttachmentEntity::class.java
-                )
-                flagOfLand.setParameter("landId", land.id)
-                flagOfLand.setParameter("landFlag", landFlag)
-                return databaseService.flagMapper().entityToModel(flagOfLand.uniqueResult())
-            }
-        } catch (e: HibernateException) {
-            LOGGER.throwing(this::class.java.simpleName, "getLandFlag", e)
-        }
-        return null
-
-    }
-
-    override fun getFlags(land: Land): List<FlagRoleAttachment> {
-        try {
-            databaseService.sessionFactory().openSession().use { session ->
-                val flagsOfLand = session.createQuery(
-                    "SELECT f FROM FlagRoleAttachmentEntity f JOIN FETCH f.land l WHERE l.id = :landId",
-                    FlagRoleAttachmentEntity::class.java
-                )
-                flagsOfLand.setParameter("landId", land.id)
-                return flagsOfLand.list().mapNotNull { databaseService.flagMapper().entityToModel(it) }
-            }
-        } catch (e: HibernateException) {
-            LOGGER.throwing(this::class.java.simpleName, "getFlags", e)
-        }
-        return emptyList()
-    }
-
 }
