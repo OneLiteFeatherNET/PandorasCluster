d3c3a78f5d4a1b03fcd9eaddab3f870719803b19
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-02-23 20:07:53 +0100

    Add PaperMC repository and update dependencies in build.gradle.kts

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,9 +6,16 @@ group = "net.onelitefeather"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
+
+    compileOnly(libs.paper)
+    implementation(project(":common"))
+    implementation(project(":api"))
+    implementation(libs.jetbrainsAnnotations)
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
