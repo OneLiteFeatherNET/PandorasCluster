5a14511a2b8d382a8ff2e0253793e3278f9e990f
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-02-23 20:09:06 +0100

    Add database dependencies to build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,8 +9,26 @@ repositories {
 }
 
 dependencies {
+
+//    implementation(libs.caffeine)
+
+    // Database
+    implementation(libs.hibernateCore)
+    implementation(libs.mariadbJavaClient)
+    implementation(libs.hibernateHikariCP)
+
+    implementation(project(":adapters:database"))
+    implementation(project(":api"))
+
+    implementation("org.glassfish.jaxb:jaxb-runtime:4.0.5")
+    implementation("org.postgresql:postgresql:42.7.4") //DATABASE
+
+    testImplementation(project(":adapters:database"))
+    testImplementation(project(":api"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
