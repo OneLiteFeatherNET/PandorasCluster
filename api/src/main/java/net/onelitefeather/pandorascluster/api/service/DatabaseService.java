9d9eb93187aa6dd73e9aa2132d640121187717ba
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-08 11:56:42 +0100

    Remove unused database entity mappers from DatabaseService interface

--- api/src/main/java/net/onelitefeather/pandorascluster/api/service/DatabaseService.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/service/DatabaseService.java
@@ -1,14 +1,5 @@
 package net.onelitefeather.pandorascluster.api.service;
 
-import net.onelitefeather.pandorascluster.api.flag.FlagContainer;
-import net.onelitefeather.pandorascluster.api.land.Land;
-import net.onelitefeather.pandorascluster.api.land.LandArea;
-import net.onelitefeather.pandorascluster.api.mapper.DatabaseEntityMapper;
-import net.onelitefeather.pandorascluster.api.player.LandPlayer;
-import net.onelitefeather.pandorascluster.dbo.flag.FlagContainerDBO;
-import net.onelitefeather.pandorascluster.dbo.land.LandAreaDBO;
-import net.onelitefeather.pandorascluster.dbo.land.LandDBO;
-import net.onelitefeather.pandorascluster.dbo.player.LandPlayerDBO;
 import org.hibernate.SessionFactory;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,17 +13,4 @@ public interface DatabaseService {
 
     @NotNull
     SessionFactory sessionFactory();
-
-    @NotNull
-    DatabaseEntityMapper<LandDBO, Land> landMapper();
-
-    @NotNull
-    DatabaseEntityMapper<LandAreaDBO, LandArea> landAreaMapper();
-
-    @NotNull
-    DatabaseEntityMapper<LandPlayerDBO, LandPlayer> landPlayerMapper();
-
-    @NotNull
-    DatabaseEntityMapper<FlagContainerDBO, FlagContainer> flagContainerMapper();
-
 }
