0165c82d5a435c868646e0a0c741e4ecb2e9c392
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-08 11:51:16 +0100

    Remove unused DATA_FOLDER constant from Constants.java

--- api/src/main/java/net/onelitefeather/pandorascluster/api/util/Constants.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/util/Constants.java
@@ -1,6 +1,5 @@
 package net.onelitefeather.pandorascluster.api.util;
 
-import java.nio.file.Path;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -10,7 +9,6 @@ public final class Constants {
     public static final UUID EVERYONE = UUID.fromString("1-1-3-3-7");
     public static final Logger LOGGER = Logger.getLogger("PandorasCluster");
     public static final Integer IGNORE_CLAIM_LIMIT = -2;
-    public static final Path DATA_FOLDER = Path.of("plugins//PandorasCluster");
 
     private Constants() {
         throw new IllegalStateException("Utility class");
