e70e10fc695f0902642f0dedf96a1dfcd64b240c
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-08 16:47:17 +0100

    Refactor DatabaseServiceImpl to use final SessionFactory for improved immutability

--- adapters/database/src/main/java/net/onelitefeather/pandorascluster/database/service/DatabaseServiceImpl.java
+++ adapters/database/src/main/java/net/onelitefeather/pandorascluster/database/service/DatabaseServiceImpl.java
@@ -1,28 +1,15 @@
 package net.onelitefeather.pandorascluster.database.service;
 
 import net.onelitefeather.pandorascluster.api.service.DatabaseService;
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.jetbrains.annotations.NotNull;
 
 public final class DatabaseServiceImpl implements DatabaseService, ThreadHelper {
 
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
     public DatabaseServiceImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-    }
-
-    @Override
-    public void connect(@NotNull String configFileResource) {
-        syncThreadForServiceLoader(() -> {
-            try {
-                sessionFactory = new Configuration().configure().configure(configFileResource).buildSessionFactory();
-            } catch (HibernateException e) {
-                throw new HibernateException("Cannot build session factory.", e);
-            }
-        });
     }
 
     @Override
