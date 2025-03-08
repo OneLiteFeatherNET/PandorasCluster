e3640e612ddfc889f743b27733d621cac01b5a5b
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-08 16:49:58 +0100

    Implement player connection listener to handle player join events

--- plugin/src/main/java/net/onelitefeather/pandorascluster/PandorasClusterPlugin.java
+++ plugin/src/main/java/net/onelitefeather/pandorascluster/PandorasClusterPlugin.java
@@ -1,11 +1,18 @@
 package net.onelitefeather.pandorascluster;
 
+import net.onelitefeather.pandorascluster.api.PandorasCluster;
+import net.onelitefeather.pandorascluster.api.PandorasClusterImpl;
+import net.onelitefeather.pandorascluster.listener.PlayerConnectionListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PandorasClusterPlugin extends JavaPlugin {
 
+    private PandorasCluster pandorasCluster;
+
     @Override
     public void onEnable() {
+        this.pandorasCluster = new PandorasClusterImpl();
+        getServer().getPluginManager().registerEvents(new PlayerConnectionListener(this.pandorasCluster), this);
         getLogger().info("PandorasCluster has been enabled!");
     }
 
---
+++ plugin/src/main/java/net/onelitefeather/pandorascluster/listener/PlayerConnectionListener.java
@@ -0,0 +1,21 @@
+package net.onelitefeather.pandorascluster.listener;
+
+import net.onelitefeather.pandorascluster.api.PandorasCluster;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerConnectionListener implements Listener {
+
+    private final PandorasCluster pandorasCluster;
+
+    public PlayerConnectionListener(PandorasCluster pandorasCluster) {
+        this.pandorasCluster = pandorasCluster;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        this.pandorasCluster.getLandPlayerService().createPlayer(event.getPlayer().getUniqueId(), event.getPlayer().getName());
+    }
+
+}
