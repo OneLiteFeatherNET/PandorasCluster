4baf0c89fa6fa39c0a1f493b554f0d07353bef7b
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-08 14:45:07 +0100

    Enhance FlagContainer with new methods for flag management and add EMPTY instance

--- api/src/main/java/net/onelitefeather/pandorascluster/api/flag/FlagContainer.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/flag/FlagContainer.java
@@ -6,13 +6,21 @@ import net.onelitefeather.pandorascluster.api.land.flag.LandNaturalFlag;
 import net.onelitefeather.pandorascluster.api.land.flag.LandRoleFlag;
 import net.onelitefeather.pandorascluster.api.mapper.PandorasModel;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class FlagContainer implements PandorasModel {
 
+    public static final FlagContainer EMPTY = new FlagContainer(
+            null,
+            null,
+            Collections.emptyList(),
+            Collections.emptyList(),
+            Collections.emptyList());
+
     private final Long id;
-    private final Land land;
+    private Land land;
     private final List<LandNaturalFlag> naturalFlags;
     private final List<LandRoleFlag> roleFlags;
     private final List<LandEntityCapFlag> entityCapFlags;
@@ -37,6 +45,11 @@ public class FlagContainer implements PandorasModel {
         return land;
     }
 
+    public FlagContainer withLand(Land land) {
+        this.land = land;
+        return this;
+    }
+
     public List<LandNaturalFlag> getNaturalFlags() {
         return naturalFlags;
     }
@@ -47,6 +60,18 @@ public class FlagContainer implements PandorasModel {
 
     public List<LandEntityCapFlag> getEntityCapFlags() {
         return entityCapFlags;
+    }
+
+    public void removeRoleFlag(LandRoleFlag roleFlag) {
+        this.roleFlags.remove(roleFlag);
+    }
+
+    public void removeNaturalFlag(LandNaturalFlag naturalFlag) {
+        this.naturalFlags.remove(naturalFlag);
+    }
+
+    public void removeEntityCapFlag(LandEntityCapFlag entityCapFlag) {
+        this.entityCapFlags.remove(entityCapFlag);
     }
 
     @Override
