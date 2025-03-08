b474eec0b9734a302fee168f6ed7ef30110e0a2d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-08 12:00:25 +0100

    Refactor DatabaseServiceImpl to remove unused mappers and simplify constructor

--- adapters/database/src/main/java/net/onelitefeather/pandorascluster/database/service/DatabaseServiceImpl.java
+++ adapters/database/src/main/java/net/onelitefeather/pandorascluster/database/service/DatabaseServiceImpl.java
@@ -1,17 +1,7 @@
 package net.onelitefeather.pandorascluster.database.service;
 
 import net.onelitefeather.pandorascluster.api.PandorasCluster;
-import net.onelitefeather.pandorascluster.api.flag.FlagContainer;
-import net.onelitefeather.pandorascluster.api.land.Land;
-import net.onelitefeather.pandorascluster.api.land.LandArea;
-import net.onelitefeather.pandorascluster.api.mapper.DatabaseEntityMapper;
-import net.onelitefeather.pandorascluster.api.player.LandPlayer;
 import net.onelitefeather.pandorascluster.api.service.DatabaseService;
-import net.onelitefeather.pandorascluster.database.mapper.impl.*;
-import net.onelitefeather.pandorascluster.dbo.flag.FlagContainerDBO;
-import net.onelitefeather.pandorascluster.dbo.land.LandAreaDBO;
-import net.onelitefeather.pandorascluster.dbo.land.LandDBO;
-import net.onelitefeather.pandorascluster.dbo.player.LandPlayerDBO;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,23 +10,9 @@ import org.jetbrains.annotations.NotNull;
 public class DatabaseServiceImpl implements DatabaseService, ThreadHelper {
 
     private SessionFactory sessionFactory;
-    private final LandMapper landMapper;
-    private final LandAreaMapper landAreaMapper;
-    private final LandPlayerMapper landPlayerMapper;
-    private final FlagContainerMapper flagContainerMapper;
 
     public DatabaseServiceImpl(PandorasCluster pandorasCluster, SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-        this.landPlayerMapper = new LandPlayerMapper();
-        this.landAreaMapper = new LandAreaMapper(this);
-        this.landMapper = new LandMapper(this.landAreaMapper, this.landPlayerMapper);
-
-        DatabaseLandFlagService flagService = (DatabaseLandFlagService) pandorasCluster.getLandFlagService();
-        this.flagContainerMapper = new FlagContainerMapper(
-                this.landMapper,
-                flagService.getNaturalFlagMapper(),
-                flagService.getRoleFlagMapper(),
-                flagService.getEntityCapFlagMapper());
     }
 
     @Override
@@ -64,25 +40,5 @@ public class DatabaseServiceImpl implements DatabaseService, ThreadHelper {
     @Override
     public @NotNull SessionFactory sessionFactory() {
         return this.sessionFactory;
-    }
-
-    @Override
-    public @NotNull DatabaseEntityMapper<LandDBO, Land> landMapper() {
-        return this.landMapper;
-    }
-
-    @Override
-    public @NotNull DatabaseEntityMapper<LandAreaDBO, LandArea> landAreaMapper() {
-        return this.landAreaMapper;
-    }
-
-    @Override
-    public @NotNull DatabaseEntityMapper<LandPlayerDBO, LandPlayer> landPlayerMapper() {
-        return this.landPlayerMapper;
-    }
-
-    @Override
-    public @NotNull DatabaseEntityMapper<FlagContainerDBO, FlagContainer> flagContainerMapper() {
-        return this.flagContainerMapper;
     }
 }
