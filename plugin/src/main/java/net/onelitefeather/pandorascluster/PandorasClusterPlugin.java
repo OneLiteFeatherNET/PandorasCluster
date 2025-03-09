affd7697d5c46d29db2d57bb6a293b52bd4a6443
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-03-09 18:32:56 +0100

    Add PaperCommandService to manage command registration and execution

--- plugin/src/main/java/net/onelitefeather/pandorascluster/PandorasClusterPlugin.java
+++ plugin/src/main/java/net/onelitefeather/pandorascluster/PandorasClusterPlugin.java
@@ -3,17 +3,30 @@ package net.onelitefeather.pandorascluster;
 import net.onelitefeather.pandorascluster.api.PandorasCluster;
 import net.onelitefeather.pandorascluster.api.PandorasClusterImpl;
 import net.onelitefeather.pandorascluster.listener.PlayerConnectionListener;
+import net.onelitefeather.pandorascluster.service.PaperCommandService;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PandorasClusterPlugin extends JavaPlugin {
 
     private PandorasCluster pandorasCluster;
+    private PaperCommandService paperCommandService;
 
     @Override
     public void onEnable() {
+
         this.pandorasCluster = new PandorasClusterImpl();
+        this.paperCommandService = new PaperCommandService(this);
+        this.paperCommandService.registerCommands();
+        // Register the service for third-party plugins to use
+        this.getServer().getServicesManager().register(PandorasCluster.class, this.pandorasCluster, this, ServicePriority.Highest);
+
         getServer().getPluginManager().registerEvents(new PlayerConnectionListener(this.pandorasCluster), this);
         getLogger().info("PandorasCluster has been enabled!");
+    }
+
+    public PandorasCluster getPandorasCluster() {
+        return pandorasCluster;
     }
 
     @Override
