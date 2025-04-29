d8d3f067df0b23ab53aaa0b5fbdac9e94de4180c
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2025-04-29 19:16:25 +0200

    Refactor FlagRegistry to remove dataFolder parameter and load flags from resources

--- api/src/main/java/net/onelitefeather/pandorascluster/api/flag/FlagRegistry.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/flag/FlagRegistry.java
@@ -9,18 +9,19 @@ import net.onelitefeather.pandorascluster.api.flag.types.EntityCapFlag;
 import net.onelitefeather.pandorascluster.api.flag.types.NaturalFlag;
 import net.onelitefeather.pandorascluster.api.flag.types.RoleFlag;
 import net.onelitefeather.pandorascluster.api.util.Constants;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
 public final class FlagRegistry {
+
+    private static FlagRegistry instance;
 
     private static final List<Flag<?>> knownFlags = new ArrayList<>();
     private static final Gson GSON = new GsonBuilder().disableHtmlEscaping().create();
@@ -53,36 +54,60 @@ public final class FlagRegistry {
                 .findFirst().orElse(null);
     }
 
-    public void loadDefaultFlags(@NotNull Path dataFolder) {
-        registerRoleFlags(dataFolder);
-        registerNaturalFlags(dataFolder);
-        registerEntityCapFlags(dataFolder);
+    public void loadDefaultFlags() {
+        registerRoleFlags();
+        registerNaturalFlags();
+        registerEntityCapFlags();
     }
 
-    private void registerEntityCapFlags(@NotNull Path dataFolder) {
-        Path roleFlagsFlagsFilePath = dataFolder.resolve("entityCapFlags.json");
-        try (BufferedReader bufferedReader = Files.newBufferedReader(roleFlagsFlagsFilePath)) {
+    public static FlagRegistry getInstance() {
+        if (instance == null) instance = new FlagRegistry();
+        return instance;
+    }
+
+    private void registerEntityCapFlags() {
+
+        String fileName = "entityCapFlags.json";
+
+        try (InputStream stream = loadRessource(fileName);
+             InputStreamReader inputStreamReader = new InputStreamReader(stream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
             knownFlags.addAll(List.of(GSON.fromJson(bufferedReader, EntityCapImpl[].class)));
         } catch (IOException e) {
             Constants.LOGGER.log(Level.SEVERE, "Cannot register default role flags.", e);
         }
     }
 
-    private void registerRoleFlags(@NotNull Path dataFolder) {
-        Path roleFlagsFlagsFilePath = dataFolder.resolve("roleFlags.json");
-        try (BufferedReader bufferedReader = Files.newBufferedReader(roleFlagsFlagsFilePath)) {
+    private void registerRoleFlags() {
+        String fileName = "roleFlags.json";
+        try (InputStream stream = loadRessource(fileName);
+             InputStreamReader inputStreamReader = new InputStreamReader(stream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
             knownFlags.addAll(List.of(GSON.fromJson(bufferedReader, RoleFlagImpl[].class)));
         } catch (IOException e) {
             Constants.LOGGER.log(Level.SEVERE, "Cannot register default role flags.", e);
         }
     }
 
-    private void registerNaturalFlags(@NotNull Path dataFolder) {
-        Path roleFlagsFlagsFilePath = dataFolder.resolve("naturalFlags.json");
-        try (BufferedReader bufferedReader = Files.newBufferedReader(roleFlagsFlagsFilePath)) {
+    private void registerNaturalFlags() {
+
+        String fileName = "naturalFlags.json";
+        try (InputStream stream = loadRessource(fileName);
+             InputStreamReader inputStreamReader = new InputStreamReader(stream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
             knownFlags.addAll(List.of(GSON.fromJson(bufferedReader, NaturalFlagImpl[].class)));
         } catch (IOException e) {
             Constants.LOGGER.log(Level.SEVERE, "Cannot register default natural flags.", e);
         }
+    }
+
+    private InputStream loadRessource(String path) throws IOException {
+        var result = FlagRegistry.class.getClassLoader().getResourceAsStream(path);
+        if (result == null) {
+            throw new IOException("Could not find resource: " + path);
+        }
+        return result;
     }
 }
