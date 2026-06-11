39ce9f015aca4c60029b2c3374a6a237e053b46b
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2026-06-11 19:38:38 +0200

    refactor(api): simplify ClaimedChunk record by removing redundant methods

--- api/src/main/java/net/onelitefeather/pandorascluster/api/chunk/ClaimedChunk.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/chunk/ClaimedChunk.java
@@ -2,14 +2,4 @@ package net.onelitefeather.pandorascluster.api.chunk;
 
 import org.jetbrains.annotations.NotNull;
 
-public record ClaimedChunk(Long id, @NotNull Long chunkIndex) {
-
-    public Long getId() {
-        return id;
-    }
-
-    @NotNull
-    public Long getChunkIndex() {
-        return chunkIndex;
-    }
-}
+public record ClaimedChunk(Long id, @NotNull Long chunkIndex) {}
