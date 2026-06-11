5d9c4e657754f213bee7bcfbe8194bd6b9bbca62
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2026-06-11 20:07:05 +0200

    refactor(api): add optional retrieval methods for LandArea and Land entities in PandorasCluster interface

--- api/src/main/java/net/onelitefeather/pandorascluster/api/PandorasCluster.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/PandorasCluster.java
@@ -1,6 +1,15 @@
 package net.onelitefeather.pandorascluster.api;
 
+import net.onelitefeather.pandorascluster.api.chunk.ClaimedChunk;
+import net.onelitefeather.pandorascluster.api.flag.FlagRegistry;
+import net.onelitefeather.pandorascluster.api.land.Land;
+import net.onelitefeather.pandorascluster.api.land.LandArea;
+import net.onelitefeather.pandorascluster.api.land.LandWorld;
+import net.onelitefeather.pandorascluster.api.player.LandPlayer;
 import net.onelitefeather.pandorascluster.api.service.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface PandorasCluster {
 
@@ -15,4 +24,18 @@ public interface PandorasCluster {
     StaffNotificationService getStaffNotification();
 
     LandAreaService getLandAreaService();
+
+    Optional<LandArea> getLandArea(long chunkKey);
+
+    Optional<LandArea> getLandArea(ClaimedChunk chunk);
+
+    Optional<Land> getLand(UUID playerId, int landId);
+
+    Optional<Land> getLand(UUID playerId);
+
+    Optional<Land> getLand(LandPlayer landPlayer);
+
+    Optional<LandWorld> getLandWorld(UUID worldId);
+
+    FlagRegistry getFlagRegistry();
 }
