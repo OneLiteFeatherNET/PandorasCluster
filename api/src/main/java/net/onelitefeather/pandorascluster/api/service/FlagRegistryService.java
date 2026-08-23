3259242e24296718b2c381be791290bf47cbf008
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2026-08-23 19:33:32 +0200

    feat(flags): add method documentation for FlagRegistryService interface

--- api/src/main/java/net/onelitefeather/pandorascluster/api/service/FlagRegistryService.java
+++ api/src/main/java/net/onelitefeather/pandorascluster/api/service/FlagRegistryService.java
@@ -8,17 +8,47 @@ import java.util.List;
 
 public interface FlagRegistryService {
 
+    /**
+     *
+     * @return a list of all known {@link EntityCapFlag}'s
+     */
     List<EntityCapFlag> getEntityCapFlags();
 
+    /**
+     *
+     * @return a list of all known {@link RoleFlag}'s
+     */
     List<RoleFlag> getRoleFlags();
 
+    /**
+     *
+     * @return a list of all known {@link NaturalFlag}'s
+     */
     List<NaturalFlag> getNaturalFlags();
 
+    /**
+     *
+     * @param name the name of the flag
+     * @return an {@link EntityCapFlag} by the giving name
+     */
     EntityCapFlag entityCapFlagOf(String name);
 
+    /**
+     *
+     * @param name the name of the flag
+     * @return an {@link RoleFlag} by the giving name
+     */
     RoleFlag roleFlagOf(String name);
 
+    /**
+     *
+     * @param name the name of the flag
+     * @return an {@link NaturalFlag} by the giving name
+     */
     NaturalFlag naturalFlagOf(String name);
 
+    /**
+     * Register all default {@link net.onelitefeather.pandorascluster.api.flag.Flag>'s }
+     */
     void registerDefaults();
 }
